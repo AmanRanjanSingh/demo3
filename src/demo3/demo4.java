80f94d3e97f349a7c14a09a4ed9cb7859565793a
Dell <Dell@DESKTOP-EFDP6JJ>
2022-09-01 12:18:29 +0530

    Commit

---
+++ src/demo3/demo4.java
@@ -0,0 +1,19 @@
+package demo3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "./synkdup/drivers/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+
+
+	}
+
+}
